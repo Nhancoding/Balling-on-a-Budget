b8e36892d07c5484ab038e71f3baa03cf51090d2
Makola001 <119084080+Makola001@users.noreply.github.com>
2023-01-12 11:28:10 -0800

    adding Countries API to JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello")
+
+// Countries API Fetch
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '86dd07a4d7msh506dceb978bc41dp168ab6jsnc3ac1a21306b',
+		'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
+	}
+};
+
+fetch('https://countries-cities.p.rapidapi.com/location/country/list', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+    

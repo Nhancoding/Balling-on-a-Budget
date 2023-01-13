74a6b89e46e6a5a09c131a86b4674410c94c3323
Nhan Duong <nhanduong@mjreseattle.com>
2023-01-13 11:13:26 -0800

    update

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 
-console.log("helooo")
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '26204d0e98msh01983f4e2c59185p114480jsn49cd66599ba4',
-		'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
+// console.log("helooo")
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '26204d0e98msh01983f4e2c59185p114480jsn49cd66599ba4',
+// 		'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
+// 	}
+// };
+
+
+var currentExh = function () {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '26204d0e98msh01983f4e2c59185p114480jsn49cd66599ba4',
+			'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
+		}
+	};
+
+	fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/USD', options)
+		.then(response => response.json())
+		.then(response => console.log(response))
+	response.json().then(function (data) {
+		console.log(data);
 	}
-};
+	)
+		.catch(err => console.error(err));
 
-fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/USD', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-console.log("hello")
+}
+currentExh();
 
 // Countries API Fetch
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '86dd07a4d7msh506dceb978bc41dp168ab6jsnc3ac1a21306b',
-		'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
-	}
-};
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '86dd07a4d7msh506dceb978bc41dp168ab6jsnc3ac1a21306b',
+// 		'X-RapidAPI-Host': 'countries-cities.p.rapidapi.com'
+// 	}
+// };
 
-fetch('https://countries-cities.p.rapidapi.com/location/country/list', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+// fetch('https://countries-cities.p.rapidapi.com/location/country/list', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 

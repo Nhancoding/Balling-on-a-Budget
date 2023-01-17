bf051d6d14966bff5819e693a3bca9caa9b2fd59
Johnny Velasco <johnnyvelasco@Johnnys-MacBook-Air.local>
2023-01-17 13:47:31 -0800

    update

--- assets/script.js
+++ assets/script.js
@@ -8,26 +8,18 @@ var dadJokesAPI = {
 };
 
 fetch('https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes',dadJokesAPI)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(function(response) {
+		return response.json();
+	})
+	.then(function(data) {
+		console.log(data);
+		for (var i=0; i < data.length; i++) {
+			var jokeText = document.querySelector("#joke")
+			jokeText.textContent = data[i].joke;
+		}
+	})
 
 
-// Exchange rate API
-var ExchangeRateAPI = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '26204d0e98msh01983f4e2c59185p114480jsn49cd66599ba4',
-		'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
-	}
-};
-
-fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/USD', ExchangeRateAPI)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-console.log("hello")
 
 // Countries API Fetch
 var countriesAPI = {
@@ -41,4 +33,60 @@ var countriesAPI = {
 fetch('https://countries-cities.p.rapidapi.com/location/country/list', countriesAPI)
 	.then(response => response.json())
 	.then(response => console.log(response))
-	.catch(err => console.error(err));         
+	.catch(err => console.error(err));
+
+
+
+const currentEx = document.querySelector("#format-input")
+const submitBtn = document.querySelector("#destinations")
+const inputAmount = document.getElementById("budget-input")
+submitBtn.addEventListener("click", function(event){
+	event.preventDefault();
+	fetchCurrencyData ()
+	// console.log(rates);
+	console.log(currentEx.value)
+
+})
+
+
+
+// Exchange rate API
+var ExchangeRateAPI = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '26204d0e98msh01983f4e2c59185p114480jsn49cd66599ba4',
+		'X-RapidAPI-Host': 'exchangerate-api.p.rapidapi.com'
+	}
+};
+function fetchCurrencyData (){
+	var resultsEx;
+	
+fetch('https://exchangerate-api.p.rapidapi.com/rapid/latest/USD', ExchangeRateAPI)
+	// const data = await resultsEx.json()
+
+	.then(response => response.json())
+	.then(response => { 
+		// return response
+		getData(response.rates)
+	}) 
+	
+	// })
+
+	// .catch(err => console.error(err));
+	// return resultsEx
+
+}
+
+
+
+function getData (ratesArray) {
+	console.log(ratesArray);
+	console.log(currentEx.value)
+	console.log(ratesArray [currentEx.value]);
+	console.log(ratesArray [currentEx.value] * inputAmount.value);
+	var conversionContainer = document.getElementById("conversion-display");
+	conversionContainer.innerHTML = "";
+	var convertedAmt = (ratesArray [currentEx.value] * inputAmount.value);
+	var conversionAmtEl = document.createElement("p");
+	conversionAmtEl.innerText = convertedAmt;
+	conversionContainer.append(conversionAmtEl);
